6bed4133edebbebb5ee07a1a6e6bff228cb34124
GibsonCS <gibson.cruzds@gmail.com>
2025-10-14 13:40:30 -0300

    chore: sales-by-match

---
+++ sales-by-match.js
@@ -0,0 +1,28 @@
+/*
+There is a large pile of socks that must be paired by color. 
+Given an array of integers representing the color of each sock, 
+determine how many pairs of socks with matching colors there are.
+
+exemple: 
+    input: ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+    output: 3
+*/
+
+function sockMerchant(ar) {
+  let pairs = 0;
+
+  const colors = new Set();
+
+  for (const i in ar) {
+    if (!colors.has(ar[i])) {
+      colors.add(ar[i]);
+    } else {
+      pairs++;
+      colors.delete(ar[i]);
+    }
+  }
+  return pairs;
+}
+
+const ar = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+console.log(sockMerchant(ar));
